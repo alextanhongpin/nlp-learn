cd74c0f316f70168d7603bab0d86b1a86235cd39
alextanhongpin <alextan220990@gmail.com>
2019-12-02 00:04:20 +0800

    chore: completed text classification chapter

---
+++ text-analytics-with-python/module/normalization.py
@@ -0,0 +1,39 @@
+import string
+import re
+from nltk.corpus import stopwords
+
+from module.lemmatize import lemmatize_text, pos_tag_text
+from module.contractions import expand_contractions
+from module.tokenize import tokenize_text
+
+stopword_list = stopwords.words('english')
+
+
+def remove_special_characters(text):
+    tokens = tokenize_text(text)
+    pattern = re.compile('[{}]'.format(re.escape(string.punctuation)))
+    filtered_tokens = filter(None,
+                             [pattern.sub('', token) for token in tokens])
+    filtered_text = ' '.join(filtered_tokens)
+    return filtered_text
+
+
+def remove_stopwords(text):
+    tokens = tokenize_text(text)
+    filtered_tokens = [token for token in tokens if token not in stopword_list]
+    filtered_text = ' '.join(filtered_tokens)
+    return filtered_text
+
+
+def normalize_corpus(corpus, tokenize=False):
+    normalized_corpus = []
+    for text in corpus:
+        text = expand_contractions(text)
+        text = lemmatize_text(text)
+        text = remove_special_characters(text)
+        text = remove_stopwords(text)
+        normalized_corpus.append(text)
+        if tokenize:
+            text = tokenize_text(text)
+            normalized_corpus.append(text)
+    return normalized_corpus
