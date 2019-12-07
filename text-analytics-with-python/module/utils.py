612b862a2d1882cc65615ff653abc10e7aa57920
alextanhongpin <alextan220990@gmail.com>
2019-12-08 02:27:27 +0800

    chore: update text clustering chapter

--- text-analytics-with-python/module/utils.py
+++ text-analytics-with-python/module/utils.py
@@ -1,11 +1,26 @@
 import html
+import re
+
 from module.contractions import expand_contractions
 from module.lemmatize import lemmatize_text
 from module.tokenize import tokenize_text
 from module.normalization import remove_special_characters, remove_stopwords
 
 
-def normalize_corpus(corpus, lemmatize=True, tokenize=False):
+def keep_text_characters(text):
+    filtered_tokens = []
+    tokens = tokenize_text(text)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    filtered_text = ' '.join(filtered_tokens)
+    return filtered_text
+
+
+def normalize_corpus(corpus,
+                     lemmatize=True,
+                     tokenize=False,
+                     only_text_chars=False):
     normalized_corpus = []
     for text in corpus:
         text = html.unescape(text)
@@ -16,6 +31,10 @@ def normalize_corpus(corpus, lemmatize=True, tokenize=False):
             text = text.lower()
         text = remove_special_characters(text)
         text = remove_stopwords(text)
+
+        if only_text_chars:
+            text = keep_text_characters(text)
+
         if tokenize:
             text = tokenize_text(text)
             normalized_corpus.append(text)
@@ -28,16 +47,25 @@ def normalize_corpus(corpus, lemmatize=True, tokenize=False):
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 
-def build_feature_matrix(documents, feature_type='frequency'):
+def build_feature_matrix(documents,
+                         feature_type='frequency',
+                         min_df=1,
+                         max_df=100,
+                         ngram_range=(1, 1)):
     feature_type = feature_type.lower().strip()
 
     if feature_type == 'binary':
-        vectorizer = CountVectorizer(binary=True, min_df=1, ngram_range=(1, 1))
+        vectorizer = CountVectorizer(
+            binary=True, min_df=min_df, max_df=max_df, ngram_range=ngram_range)
     elif feature_type == 'frequency':
         vectorizer = CountVectorizer(
-            binary=False, min_df=1, ngram_range=(1, 1))
+            binary=False,
+            min_df=min_df,
+            max_df=max_df,
+            ngram_range=ngram_range)
     elif feature_type == 'tfidf':
-        vectorizer = TfidfVectorizer(min_df=1, ngram_range=(1, 1))
+        vectorizer = TfidfVectorizer(
+            min_df=min_df, max_df=max_df, ngram_range=ngram_range)
     else:
         raise Exception(
             'Wrong feature type entered. Possible values: "binary", "frequency", "tfidf"'
