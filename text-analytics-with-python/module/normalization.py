cae7146bc9f724a4d496cff8fab0cf3bb5cdb602
alextanhongpin <alextan220990@gmail.com>
2019-12-08 13:19:31 +0800

    chore: update chapter sentiment analysis

--- text-analytics-with-python/module/normalization.py
+++ text-analytics-with-python/module/normalization.py
@@ -1,6 +1,10 @@
-import string
-import re
+from html.parser import HTMLParser
 from nltk.corpus import stopwords
+import html
+import nltk
+import re
+import string
+import unicodedata
 
 from module.lemmatize import lemmatize_text, pos_tag_text
 from module.contractions import expand_contractions
@@ -25,15 +29,78 @@ def remove_stopwords(text):
     return filtered_text
 
 
-def normalize_corpus(corpus, tokenize=False):
+def parse_document(document):
+    document = re.sub('\n', ' ', document)
+    if isinstance(document, str):
+        document = document
+    elif isinstance(document, unicode):
+        return unicodedata.normalize('NFKD', document).encode(
+            'ascii', 'ignore')
+    else:
+        raise ValueError('Document is not string or unicode!')
+    document = document.strip()
+    sentences = nltk.sent_tokenize(document)
+    sentences = [sentence.strip() for sentence in sentences]
+    return sentences
+
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.fed = []
+
+    def handle_data(self, d):
+        self.fed.append(d)
+
+    def get_data(self):
+        return ' '.join(self.fed)
+
+
+def strip_html(text):
+    html_stripper = MLStripper()
+    html_stripper.feed(text)
+    return html_stripper.get_data()
+
+
+def normalize_accented_characters(text):
+    text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore')
+    return text
+
+
+def keep_text_characters(text):
+    filtered_tokens = []
+    tokens = tokenize_text(text)
+    for token in tokens:
+        if re.search('[a-zA-Z]', token):
+            filtered_tokens.append(token)
+    filtered_text = ' '.join(filtered_tokens)
+    return filtered_text
+
+
+def normalize_corpus(corpus,
+                     lemmatize=True,
+                     only_text_chars=False,
+                     tokenize=False):
     normalized_corpus = []
     for text in corpus:
+        text = normalize_accented_characters(text)
+        text = html.unescape(text.decode('utf-8'))
+        text = strip_html(text)
         text = expand_contractions(text)
-        text = lemmatize_text(text)
+        if lemmatize:
+            text = lemmatize_text(text)
+        else:
+            text = text.lower()
+
         text = remove_special_characters(text)
         text = remove_stopwords(text)
-        normalized_corpus.append(text)
+        if only_text_chars:
+            text = keep_text_characters(text)
+
         if tokenize:
             text = tokenize_text(text)
-            normalized_corpus.append(text)
+
+        normalized_corpus.append(text)
+
     return normalized_corpus
--- text-analytics-with-python/module/utils.py
+++ text-analytics-with-python/module/utils.py
@@ -73,3 +73,59 @@ def build_feature_matrix(documents,
 
     feature_matrix = vectorizer.fit_transform(documents).astype(float)
     return vectorizer, feature_matrix
+
+
+from sklearn import metrics
+import numpy as np
+import pandas as pd
+
+
+def display_evaluation_metrics(true_labels, predicted_labels,
+                               positive_class=1):
+    print('Accuracy:',
+          np.round(metrics.accuracy_score(true_labels, predicted_labels), 2))
+    print(
+        'Precision:',
+        np.round(
+            metrics.precision_score(
+                true_labels,
+                predicted_labels,
+                pos_label=positive_class,
+                average='binary'), 2))
+    print(
+        'Recall:',
+        np.round(
+            metrics.recall_score(
+                true_labels,
+                predicted_labels,
+                pos_label=positive_class,
+                average='binary'), 2))
+    print(
+        'F1 Score:',
+        np.round(
+            metrics.f1_score(
+                true_labels,
+                predicted_labels,
+                pos_label=positive_class,
+                average='binary'), 2))
+
+
+def display_confusion_matrix(true_labels, predicted_labels, classes=[1, 0]):
+    cm = metrics.confusion_matrix(
+        y_true=true_labels, y_pred=predicted_labels, labels=classes)
+    cm_frame = pd.DataFrame(
+        data=cm,
+        columns=pd.MultiIndex(
+            levels=[['Predicted:'], classes], labels=[[0, 0], [0, 1]]),
+        index=pd.MultiIndex(
+            levels=[['Actual:'], classes], labels=[[0, 0], [0, 1]]))
+
+    print(cm_frame)
+
+
+def display_classification_report(true_labels,
+                                  predicted_labels,
+                                  classes=[1, 0]):
+    report = metrics.classification_report(
+        y_true=true_labels, y_pred=predicted_labels, labels=classes)
+    print(report)
